a5b52a8472590d688a7c2535e6c3841972211e32
LukisG <lukas.moderator@gmail.com>
2022-06-22 23:06:16 +0300

    added nothing entered error, fixed crash

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,8 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { GlobalTotal } from "./GlobalVariables";
 import { Input, Button, Alert } from "antd";
 const InputSection = ({ setInput, handlesubmit, input, outofbudget }) => {
   const GlobalArray = useContext(GlobalTotal);
+  const [nothing, setNothing] = useState(false);
+  const [nothingEntered, setNothingEntered] = useState(false);
   return (
     <>
       <Input
@@ -10,24 +12,35 @@ const InputSection = ({ setInput, handlesubmit, input, outofbudget }) => {
         id="value"
         onChange={(e) => {
           setInput(e.target.value);
+          console.log(e.target.value);
         }}
         style={{ width: 205 }}
         type="number"
       />
       <Button
         type="primary"
-        onClick={input ? handlesubmit : console.log("nothing entered")}
+        onClick={
+          input == 0
+            ? () => setNothing(true)
+            : () =>
+                handlesubmit() + setNothing(false) + setNothingEntered(false)
+        }
       >
         ADD
       </Button>
-      {outofbudget ? (
+      {outofbudget && (
         <Alert
           message="Out of Budget my friend :("
           type="error"
           style={{ width: 300 }}
         />
-      ) : (
-        <></>
+      )}
+      {nothing && (
+        <Alert
+          message="Enter something please, I need a number for snacs!"
+          type="success"
+          style={{ width: 300 }}
+        />
       )}
     </>
   );
